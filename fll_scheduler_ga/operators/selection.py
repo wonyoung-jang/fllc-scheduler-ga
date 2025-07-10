4c4b4bc38d56a78c42e190ce3bf09884c18f2337
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2025-07-10 13:27:13 -0400

    refactor: simplify selection logic in TournamentSelectionNSGA2 and ElitismSelectionNSGA2 classes

--- fll_scheduler_ga/operators/selection.py
+++ fll_scheduler_ga/operators/selection.py
@@ -27,8 +27,8 @@ class TournamentSelectionNSGA2(Selection):
     def select(self, population: Population, num_parents: int) -> Iterator[Schedule]:
         """Select individuals using NSGA-II tournament selection."""
         for _ in range(num_parents):
-            participants = self.rng.sample(population, k=self.ga_parameters.selection_size)
-            yield min(participants, key=lambda p: (p.rank, -p.crowding))
+            tournament = self.rng.sample(population, self.ga_parameters.selection_size)
+            yield min(tournament, key=lambda p: (p.rank, -p.crowding))
 
 
 @dataclass(slots=True)
@@ -40,28 +40,4 @@ class ElitismSelectionNSGA2(Selection):
 
     def select(self, population: Population, population_size: int) -> Iterator[Schedule]:
         """Select the new generation based on non-dominated sorting and crowding distance."""
-        population.sort(key=lambda p: p.rank)
-
-        new_pop = []
-        last_rank_start_index = 0
-        for i, p in enumerate(population):
-            if p.rank != population[last_rank_start_index].rank:
-                current_front = population[last_rank_start_index:i]
-                if len(new_pop) + len(current_front) > population_size:
-                    current_front.sort(key=lambda ind: ind.crowding, reverse=True)
-                    remaining_size = population_size - len(new_pop)
-                    new_pop.extend(current_front[:remaining_size])
-                    yield from new_pop
-                    return
-                new_pop.extend(current_front)
-                last_rank_start_index = i
-
-        last_front = population[last_rank_start_index:]
-        if len(new_pop) + len(last_front) > population_size:
-            last_front.sort(key=lambda ind: ind.crowding, reverse=True)
-            remaining_size = population_size - len(new_pop)
-            new_pop.extend(last_front[:remaining_size])
-        else:
-            new_pop.extend(last_front)
-
-        yield from new_pop
+        yield from population[:population_size]
