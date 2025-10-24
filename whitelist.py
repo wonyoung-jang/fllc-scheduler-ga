18ce59b00b34395de876e7f012cee93ae1b1a12d
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2025-10-24 17:47:03 -0400

    chore: update dependencies and vulture whitelisting

---
+++ whitelist.py
@@ -0,0 +1,10 @@
+"""Whitelist for vulture to avoid false positives."""
+
+from fll_scheduler_ga.api.main import app
+from fll_scheduler_ga.config.constants import FitnessObjective
+
+app  # noqa: B018
+
+FitnessObjective.BREAK_TIME  # noqa: B018
+FitnessObjective.LOCATION_CONSISTENCY  # noqa: B018
+FitnessObjective.OPPONENT_VARIETY  # noqa: B018
