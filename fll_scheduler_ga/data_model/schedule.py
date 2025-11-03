fix: team normalization on export

fll_scheduler_ga/data_model/schedule.py
@@ -164,16 +164,13 @@ class Schedule:
         return np.nonzero(self.schedule == -1)[0]
 
     def normalized_teams(self) -> np.ndarray:
-        """Normalize the schedule by reassigning team identities."""
-        normalized = [None] * len(Schedule.teams)
+        """Normalize the schedule by reassigning team identities based on their order of appearance."""
+        normalized = np.array([-1] * len(Schedule.teams), dtype=int)
         count = 1
         for team in self.schedule:
-            if team in normalized:
+            if normalized[team] != -1:
                 continue
 
             normalized[team] = self.team_identities.get(count, count)
-            if count == len(self.teams):
-                break
-
             count += 1
-        return np.array(normalized)
+        return normalized