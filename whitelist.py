1b6be3bd3357b71b702fb5aeb2b61816f5e19022
Wonyoung Jang <114602935+wonyoung-jang@users.noreply.github.com>
2025-11-28 21:42:29 -0500

    chore: update vulture whitelist

--- whitelist.py
+++ whitelist.py
@@ -1,11 +1,15 @@
 """Whitelist for vulture to avoid false positives."""
 
-from fll_scheduler_ga.api.main import app
-
 from fll_scheduler_ga.config.constants import FitnessObjective
-
-app  # noqa: B018
+from fll_scheduler_ga.config.schemas import GaParameters, TournamentRound
+from fll_scheduler_ga.data_model.event import Event, EventProperties
 
 FitnessObjective.BREAK_TIME  # noqa: B018
 FitnessObjective.LOCATION_CONSISTENCY  # noqa: B018
 FitnessObjective.OPPONENT_VARIETY  # noqa: B018
+
+GaParameters.validate  # noqa: B018
+TournamentRound.validate_slots_empty  # noqa: B018
+
+EventProperties.all_props  # noqa: B018
+Event.model_config  # noqa: B018
