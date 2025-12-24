fix: correct spelling of 'opponents' in fitness classes and optimize hash calculation in Schedule

fll_scheduler_ga/fitness/fitness_base.py
@@ -48,7 +48,7 @@ class FitnessBase(ABC):
     _paired_idx: np.ndarray = field(default_factory=lambda: np.array([]))
     _roundtype_idx: np.ndarray = field(default_factory=lambda: np.array([]))
     # FitnessBenchmark
-    benchmark_oppoenents: np.ndarray = field(default_factory=lambda: np.array([]))
+    benchmark_opponents: np.ndarray = field(default_factory=lambda: np.array([]))
     benchmark_best_timeslot_score: float = 0.0
     # FitnessModel
     loc_weight_rounds_inter: float = 0.0
@@ -83,7 +83,7 @@ class FitnessBase(ABC):
         self._paired_idx = _ep.paired_idx
         self._roundtype_idx = _ep.roundtype_idx
         # Initialize from FitnessBenchmark
-        self.benchmark_oppoenents = self.benchmark.opponents
+        self.benchmark_opponents = self.benchmark.opponents
         self.benchmark_best_timeslot_score = self.benchmark.best_timeslot_score
         # Initialize from FitnessModel
         self.loc_weight_rounds_inter = self.model.loc_weight_rounds_inter

fll_scheduler_ga/fitness/fitness_population.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 from logging import getLogger
 
 import numpy as np
+from line_profiler import profile
 
 from .fitness_base import FitnessBase
 
@@ -27,9 +28,6 @@ class FitnessEvaluator(FitnessBase):
             np.ndarray: All team scores for the population. Shape (pop_size, num_teams, num_objectives).
 
         """
-        # Dims for reference
-        n_pop, _ = arr.shape
-
         # Get team-events mapping for the entire population
         team_events = self.get_team_events(arr)
 
@@ -39,7 +37,7 @@ class FitnessEvaluator(FitnessBase):
         )
 
         # Preallocate arrays
-        team_fits_shape = (n_pop, self.n_teams, self.n_objectives)
+        team_fits_shape = (arr.shape[0], self.n_teams, self.n_objectives)
         team_fitnesses = np.zeros(team_fits_shape, dtype=float)
 
         # Calculate scores for each objective
@@ -54,9 +52,8 @@ class FitnessEvaluator(FitnessBase):
 
     def get_team_events(self, arr: np.ndarray) -> np.ndarray:
         """Invert the (event -> team) mapping to a (team -> events) mapping for the entire population."""
-        n_pop, _ = arr.shape
-
         # Preallocate the team-events array
+        n_pop = arr.shape[0]
         team_events = np.full((n_pop, self.n_teams, self.n_max_events), -1, dtype=int)
 
         # Get indices of scheduled events
@@ -159,6 +156,7 @@ class FitnessEvaluator(FitnessBase):
 
         return final_scores / self.benchmark_best_timeslot_score
 
+    @profile
     def score_loc_consistency(self, loc_ids: np.ndarray, roundtype_ids: np.ndarray) -> np.ndarray:
         """Calculate location consistency score, prioritizing inter-round over intra-round consistency."""
         n_pop, n_teams, _ = loc_ids.shape
@@ -266,4 +264,4 @@ class FitnessEvaluator(FitnessBase):
         unique_counts = (changes & valid_mask).sum(axis=2)
         unique_counts = unique_counts + 1 if self.n_single_rt == 0 else unique_counts
 
-        return self.benchmark_oppoenents[unique_counts]
+        return self.benchmark_opponents[unique_counts]

fll_scheduler_ga/fitness/fitness_schedule.py
@@ -104,11 +104,8 @@ class FitnessEvaluatorSingle(FitnessBase):
         stop_cycle_curr = stops_cycle_sorted[:, :-1]
 
         # Calculate break durations in minutes
-        breaks_active_seconds = np.subtract(start_next, stop_active_curr)
-        breaks_active_minutes = breaks_active_seconds / 60
-
-        breaks_cycle_seconds = np.subtract(start_next, stop_cycle_curr)
-        breaks_cycle_minutes = breaks_cycle_seconds / 60
+        breaks_active_minutes = np.subtract(start_next, stop_active_curr) / 60
+        breaks_cycle_minutes = np.subtract(start_next, stop_cycle_curr) / 60
 
         # Identify overlaps
         overlap_mask = (breaks_cycle_minutes < 0).any(axis=1)
@@ -155,7 +152,6 @@ class FitnessEvaluatorSingle(FitnessBase):
     def score_loc_consistency(self, loc_ids: np.ndarray, roundtype_ids: np.ndarray) -> np.ndarray:
         """Calculate location consistency score, prioritizing inter-round over intra-round consistency."""
         n_teams, _ = loc_ids.shape
-        match_roundtypes = self.match_roundtypes
         shape = n_teams
 
         # Consistency score is only meaningful with 1+ match round types
@@ -168,9 +164,9 @@ class FitnessEvaluatorSingle(FitnessBase):
         if max_loc_idx < 0:
             return np.ones(shape, dtype=float)
 
-        max_rt_id = max(roundtype_ids.max(), match_roundtypes.max())
+        max_rt_id = max(roundtype_ids.max(), self.match_roundtypes.max())
         is_match_rt_lookup = np.zeros(max_rt_id + 1, dtype=bool)
-        is_match_rt_lookup[match_roundtypes] = True
+        is_match_rt_lookup[self.match_roundtypes] = True
         match_rt_mask = is_match_rt_lookup[roundtype_ids] & (loc_ids >= 0)
 
         team_indices, _ = match_rt_mask.nonzero()
@@ -258,4 +254,4 @@ class FitnessEvaluatorSingle(FitnessBase):
         unique_counts = (changes & valid_mask).sum(axis=1)
         unique_counts = unique_counts + 1 if self.n_single_rt == 0 else unique_counts
 
-        return self.benchmark_oppoenents[unique_counts]
+        return self.benchmark_opponents[unique_counts]